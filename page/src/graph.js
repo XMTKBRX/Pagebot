0d6aa0588b9437c4849d8005906cb8521034f989
XMTKBRX <160078586+XMTKBRX@users.noreply.github.com>
2025-05-02 20:19:28 +0200

    Update graph.js

--- page/src/graph.js
+++ page/src/graph.js
@@ -5,7 +5,7 @@ module.exports = function (event) {
     // Directly return the axios promise
     return axios
       .post(
-        `https://graph.facebook.com/v20.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`,
+        `https://graph.facebook.com/v20.0/me/messages?access_token=${EAAUYi3QZAJpgBO0ZCeA87HAJIT803HQL4Ed2q5Fep0VKRHjepwfUe0hwwLYYOGdE5f7laMqIEJhH0ynSdIJu6PJUpsxZCzw9iXPfy6uHmAFiZAA7NquD181PhYrP1H3ym5gqMwSlFM4owD99R9g4ZCEtiyLdNvBZC2DsgLigVmChkrR0tFAvNNuHs5k1VqugZDZD}`,
         form
       )
       .then((res) => res.data) // Return the response data
