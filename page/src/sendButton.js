de23bc89f39da7556d2ab562ff0cc73093b1ffc2
XMTKBRX <160078586+XMTKBRX@users.noreply.github.com>
2025-05-02 20:17:38 +0200

    Update sendButton.js

--- page/src/sendButton.js
+++ page/src/sendButton.js
@@ -35,7 +35,7 @@ module.exports = function (event) {
   function Graph(form) {
     return axios
       .post(
-        `https://graph.facebook.com/v20.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`,
+        `https://graph.facebook.com/v20.0/me/messages?access_token=${EAAUYi3QZAJpgBO0ZCeA87HAJIT803HQL4Ed2q5Fep0VKRHjepwfUe0hwwLYYOGdE5f7laMqIEJhH0ynSdIJu6PJUpsxZCzw9iXPfy6uHmAFiZAA7NquD181PhYrP1H3ym5gqMwSlFM4owD99R9g4ZCEtiyLdNvBZC2DsgLigVmChkrR0tFAvNNuHs5k1VqugZDZD}`,
         form
       )
       .then((res) => res.data)
